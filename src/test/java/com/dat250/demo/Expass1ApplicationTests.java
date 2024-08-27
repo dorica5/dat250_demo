adf82b9b5ea322abddcb66c61a939f2dc3d6c76e
Dorcas Mihio <dorcasmihio@gmail.com>
2024-08-27 19:42:58 +0200

    Initial commit of demo project

---
+++ src/test/java/com/dat250/demo/Expass1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dat250.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Expass1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
